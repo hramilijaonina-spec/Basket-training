42096ee7e422177e5d552c0e96013be9d42fbd24
hramilijaonina-spec <h.ramilijaonina@gmail.com>
2026-09-21 08:33:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,18 @@
 /* Détente PWA — service worker (network-first, tolérant) */
-const CACHE = "detente-m1-1.14";
+const CACHE = "detente-m1-1.15";
 const ASSETS = [
-  "./", "./index.html", "./manifest.webmanifest", "./icon.svg",
-  "./hero.jpg",
-  "./ex-squat.jpg", "./ex-souleve.jpg", "./ex-fente.jpg", "./ex-mollets.jpg",
-  "./ex-deadbug.jpg", "./ex-drop.jpg", "./ex-epaules.jpg", "./ex-planche.jpg",
-  "./ex-pogos.jpg", "./ex-pompes.jpg", "./ex-rowing.jpg", "./ex-stepup.jpg"
+  "./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./hero.jpg",
+  "./ex-squat.jpg", "./ex-fente.jpg", "./ex-souleve.jpg", "./ex-mollets.jpg",
+  "./ex-drop.jpg", "./ex-pogos.jpg", "./ex-planche.jpg", "./ex-pompes.jpg",
+  "./ex-rowing.jpg", "./ex-epaules.jpg", "./ex-stepup.jpg", "./ex-deadbug.jpg",
+  "./ex-gainlat.jpg", "./ex-wallsit.jpg", "./ex-squatjump.jpg", "./ex-broadjump.jpg",
+  "./ex-skips.jpg", "./ex-fentesaut.jpg", "./ex-molletiso.jpg", "./ex-hollow.jpg"
 ];
 
 self.addEventListener("install", e => {
   e.waitUntil((async () => {
     const c = await caches.open(CACHE);
-    await Promise.all(ASSETS.map(u => c.add(u).catch(() => {})));
+    await Promise.all(ASSETS.map(u => c.add(u).catch(() => {})));  // fichier manquant ignoré
     self.skipWaiting();
   })());
 });
